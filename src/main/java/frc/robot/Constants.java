c13d8d8ee1918bf47d731ab9eca4d947249f06b3
Abbey Sieg <abbeys2025@gmail.com>
2024-01-29 18:16:02 -0600

    new swerve library

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,272 +4,49 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.swervelib.math.Matter;
+import frc.robot.swervelib.parser.PIDFConfig;
 
-/* Table of Contents:
- *    Availablility of Subsystems
- *    ModuleConstants
- *    DriveConstants
- *    CANIDs
- *    ArmConstants
- *    AutoConstants
- *    OIConstants
- *    CamConstant
- *    Pneumatics
- *    RobotConstants
- *    FieldConstants
- *    GripperConstants
- *    Lights
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
+public final class Constants
+{
 
-public final class Constants {
+  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
-  public static final boolean PIXY_AVAILABLE = true;
- 
-  
-  //public static final int SYSTEMCHOOSER = 0;
+  public static final class Auton
+  {
 
+    public static final PIDFConfig TranslationPID     = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
 
-    public static final class ModuleConstants {
-      public static final double kWheelDiameterMeters = Units.inchesToMeters(3.75);
-      public static final double kDriveMotorGearRatio = 1 / 6.75;
-      public static final double kTurningMotorGearRatio = 1 / 12.8;
-      public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
-      public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
-      public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 42;
-      public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 42;
-      public static final double kPTurning = 0.5;
-     
-     // public static final double kDriveEncoderRot2Meter_Comp = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
-     // Theoraticlly maybe; in practice from measurements
-      
-      
-    }
+    public static final double MAX_ACCELERATION = 2;
+  }
 
-    public static final class DriveConstants {
-        public static final double kTrackWidth = Units.inchesToMeters(21.5);
-        // Distance between right and left wheels
-        public static final double kWheelBase = Units.inchesToMeters(24.75);
-        // Distance between front and back wheels
-       
-        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-//                new Translation2d( kWheelBase / 2, -kTrackWidth / 2),
-  //              new Translation2d( kWheelBase / 2,  kTrackWidth / 2),
-    //            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
-      //          new Translation2d(-kWheelBase / 2,  kTrackWidth / 2));
+  public static final class Drivebase
+  {
 
-                new Translation2d( kWheelBase / 2, -kTrackWidth / 2),
-                new Translation2d( kWheelBase / 2,  kTrackWidth / 2),
-                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
-                new Translation2d(-kWheelBase / 2,  kTrackWidth / 2));
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
 
-       
-                  
+  public static class OperatorConstants
+  {
 
-//    COMP BOT  FRONT                PRAC BOT    FRONT
-//     +----------------------+        +----------------------+
-//     | D11 S21      D12 S22 |        | D15 S25      D16 S26 |
-//     | E31          E32     |        | E35          E36     |
-//     |                      |        |                      |
-//     |                      |        |                      |
-//     |                      |        |                      |
-//     |                      |        |                      |
-//     | D13 S23      D14 S24 |        | D17 S27      D18 S28 |
-//     | E33          E34     |        | E37          E38     |
-//     +----------------------+        +----------------------+
-//
-        public static final int kFrontLeftDriveMotorPort        = 1;    // Module 1
-        public static final int kFrontLeftTurningMotorPort      = 2;
-        public static final int kFrontLeftAbsoluteEncoderPort   = 0;
-
-        public static final int kFrontRightDriveMotorPort       = 5;    // Module 2
-        public static final int kFrontRightTurningMotorPort     = 6;
-        public static final int kFrontRightAbsoluteEncoderPort  = 2;
-
-        public static final int kBackRightDriveMotorPort        = 7;    // Module 3
-        public static final int kBackRightTurningMotorPort      = 8;
-        public static final int kBackRightAbsoluteEncoderPort   = 3;
-
-        public static final int kBackLeftDriveMotorPort         = 3;    // Module 4
-        public static final int kBackLeftTurningMotorPort       = 4;
-        public static final int kBackLeftAbsoluteEncoderPort    = 1;
-
-        
-
-        public static final boolean kFrontLeftTurningEncoderReversed  = true;
-        public static final boolean kBackLeftTurningEncoderReversed   = true;
-        public static final boolean kFrontRightTurningEncoderReversed = true;
-        public static final boolean kBackRightTurningEncoderReversed  = true;
-
-        public static final boolean kFrontLeftDriveEncoderReversed  = false;
-        public static final boolean kBackLeftDriveEncoderReversed   = false;
-        public static final boolean kFrontRightDriveEncoderReversed = false;
-        public static final boolean kBackRightDriveEncoderReversed  = false;
-
-        public static final boolean kFrontLeftDriveAbsoluteEncoderReversed  = true;
-        public static final boolean kBackLeftDriveAbsoluteEncoderReversed   = true;
-        public static final boolean kFrontRightDriveAbsoluteEncoderReversed = true;
-        public static final boolean kBackRightDriveAbsoluteEncoderReversed  = true;
-
-        public static final double kFrontLeftAbsoluteEncoderOffset = -3.734; 
-        public static final double kBackLeftAbsoluteEncoderOffset = -4.529;
-        public static final double kFrontRightAbsoluteEncoderOffset = 3.299;
-        public static final double kBackRightAbsoluteEncoderOffset = 2.600;
-
-        public static final double kPhysicalMaxSpeedMetersPerSecond = 1;
-        public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
-        
-
-        public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond /1.5;
-        public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = 
-                                           kPhysicalMaxAngularSpeedRadiansPerSecond / 4;
-        public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 3;
-        public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 1.8;
-        public static final double kptwist = .5;
-  
-    //    CAN_Number = new int[10];
-    //    CAN_Name   = new string[30];
-
-     //   CAN_Number[1] = 11;
-     //   CAN_Name[1]   = "Front Left Drive Motor";
-     //   CAN_Number[2] = 21;
-     //   CAN_Name[2]   = "Front Left Turn Motor";
-     //   CAN_Number[3] = 31;
-     //   CAN_Name[3]   = "Front Left Absolute Encoder";
-
-      //  CAN_Number[4] = 12;
-      //  CAN_Name[4]   = "Front Right Drive Motor";
-      //  CAN_Number[5] = 22;
-      //  CAN_Name[5]   = "Front Right Turn Motor";
-     //   CAN_Number[6] = 32;
-      //  CAN_Name[6]   = "Front Right Absolute Encoder";
-
-     //   CAN_Number[7] = 13;
-     //   CAN_Name[7]   = "Back Right Drive Motor";
-     //   CAN_Number[8] = 23;
-     //   CAN_Name[8]   = "Back Right Turn Motor";
-     //   CAN_Number[9] = 33;
-     //   CAN_Name[9]   = "Back Right Absolute Encoder";
-
-      //  CAN_Number[10] = 14;
-     //   CAN_Name[10]   = "Back Left Drive Motor";
-     //   CAN_Number[11] = 24;
-     //   CAN_Name[11]   = "Back Left Turn Motor";
-     //   CAN_Number[12] = 34;
-    //    CAN_Name[12]   = "Back Left Absolute Encoder";
-
-      //  CAN_Number[13] = 99;
-
-    }
-
-    public static final class CANIDs {
-  
-    }
- 
-    public static final class AutoConstants {
-        public static final double kMaxSpeedMetersPerSecond = DriveConstants.kPhysicalMaxSpeedMetersPerSecond/1 ;
-        public static final double kMaxAngularSpeedRadiansPerSecond = 
-                                DriveConstants.kPhysicalMaxAngularSpeedRadiansPerSecond / 4;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 4;
-        public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI / 4;
-        public static final double kPXController = .0;   // 1.5;
-        public static final double kPYController = .0;    // 1.5;
-        public static final double kPThetaController = .0;  //.06; // 3;
-
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = //
-                new TrapezoidProfile.Constraints(
-                        kMaxAngularSpeedRadiansPerSecond,
-                        kMaxAngularAccelerationRadiansPerSecondSquared);
-        public static final double kPBalancer = .6;  // sin -> Vbus
-        public static final double Balancemultiplier = .55;
-    }
-
-    public static final class OIConstants {
-        public static final int kDriverControllerPort  = 0; // 1st driver
-        public static final int kDriverControllerPort2 = 1; // 2nd driver
-  //    public static final int kDRiverCOntrollerPort3 = 2; // buttonBox2
-        
-  //      public static final int kDriverControllerPort4 = 3; // ti launchpad
-  public static final double kDeadband = 0.05;
-//we need to change these ^^
-
-
-// Botton Box 0
-      
-//        public static final int kHighGridPos_BB0      =  1; 
-//        public static final int kMidGridPos_BB0       =  2; 
-//        public static final int kFloorPos_BB0         =  3; 
-//        public static final int kSubStationPos_BB0    =  4; 
-//        public static final int kRetractPos_BB0       =  5; 
-//        public static final int kOverrideEncoders_BB0 =  6; 
-//        public static final int kResetEncoders_BB0    =  7; 
-//        public static final int kSpareOne_BB0         =  8; 
-//        public static final int kNotWiredOne_BB0      =  9; 
-//        public static final int kNotWiredTwo_BB0      = 10; 
-
-// Botton Box 1
-
-//        public static final int targetRetractNudge_BB1        = 7; // buttonBox2
-//        public static final int targetExtendNudge_BB1         = 6; // buttonBox2
-//        public static final int targetRaiseNudge_BB1          = 3; // buttonBox2
-//        public static final int targetLowerNudge_BB1          = 2; // buttonBox2
-//        public static final int kgripperclosebutton_BB1       = 4; // buttonBox2
-//        public static final int PRESSURESwitch_BB1            = 12;// buttonbox2
-//        public static final int kgripperopenbutton_BB1        = 5; // ButtonBox2
-        //public static final int kgetRobotPositionButton       = 0; // buttonBox2
-        //public static final int kDrivertostationbutton        = 0; // buttonBox2
-        //public static final int kgetAprilTagButton            = 0; // buttonBox
-        //public static final int kDriveGenericx                = 0; // buttonBox
-        //public static final int kDriveGenericy                = 0; // buttonBox
-        //public static final int kDriveGenericxy               = 0; // buttonBox
-        //public static final int kEndDriveGeneric              = 0; // buttonBox
-
-        public static final int kDriverYAxis                  = 0;
-        public static final int kDriverXAxis                  = 1;
-        public static final int kDriverRotAxis                = 4;
-        public static final int fineControlAxis               = 2;
-        public static final int kDriverResetGyroButtonIdx     = 1; // driverJoytick button A
-        public static final int kDriverResetOdometryButtonIdx = 3; // driverJoytick button X
-        public static final int BALANCE_AUGMENTER             = 4; //  driver stick Y button
-        public static final int kDriverFieldOrientedButtonIdx = 5; // driverJoytick button left-bumper
-        public static final int PixyFollowButton              = 6; // driverJoytick button right-bumper
-
-        public static final int choiceswitch                  = 1; // ti launchpad axis
-        public static final int delaySwitch                   = 2; // ti launchpad axis
-        public static final int levelSwitch                   = 3; // ti launchpad axis
-        public static final int aprilTagLight                 = 1; // TI launchpad output
-        public static final int gameObjectLight               = 2; // TI launchpad output
-
-        // test mode buttonbox buttons
-
-
-   
-
-    public static final class RobotConstants {
-      public static final double xcg = Units.inchesToMeters(0);
-    }
-
-    public static final class FieldConstants {
-      public static final double chargingstationwidth = Units.inchesToMeters(48.+13.11*2); // Width of the charging station.
-      public static final double leaveCommunityDist = Units.inchesToMeters(140.+5);  // distance from Grid to farthest edge of Community
-      public static final double Halflength = Units.feetToMeters(11);
-      public static final double Gridtostation = Units.feetToMeters(6.);
-
-    }
+    // Joystick Deadband
+    public static final double LEFT_X_DEADBAND = 0.1;
+    public static final double LEFT_Y_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double TURN_CONSTANT = 0.75;
   }
 }
-  
-
-
-
-/* +---------------------------------------------------------------------------------+
-   |  Absolute Encoder Offsets (Degrees) these values are set in the CANcoders and   |
-   |                                  are recorded here for backup purposes          |
-   |    Module 0  Front Left    -20.83                                               |
-   |           1  Back Left    -188.17                                               |
-   |           2  Front Right  -287.40                                               |
-   |           3  Back Right   -281.33                                               |
-   +---------------------------------------------------------------------------------+  */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,61 +4,110 @@
 
 package frc.robot;
 
-
-import frc.robot.commands.Autos;
-import frc.robot.subsystems.SwerveSubsystem;
-
-import com.kauailabs.navx.frc.AHRS;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.OIConstants;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteDrive;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteFieldDrive;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
+import frc.robot.commands.swervedrive.drivebase.TeleopDrive;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.io.File;
+
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer {
-  private final SwerveSubsystem swerveSubsystem;
-  private AHRS gyro; 
+public class RobotContainer
+{
 
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                         "swerve/swerve"));
+  // CommandJoystick rotationController = new CommandJoystick(1);
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  CommandJoystick driverController = new CommandJoystick(1);
 
-  private final Joystick driverJoystick = new Joystick(OIConstants.kDriverControllerPort); //first driver 
-  //private final Joystick secondriverJoystick = new Joystick(OIConstants.kDriverControllerPort2); second driver
-  SendableChooser<Command> m_chooser;
+  // CommandJoystick driverController   = new CommandJoystick(3);//(OperatorConstants.DRIVER_CONTROLLER_PORT);
+  XboxController driverXbox = new XboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-
-    swerveSubsystem = new SwerveSubsystem();
-    gyro = new AHRS();
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer()
+  {
     // Configure the trigger bindings
     configureBindings();
-    m_chooser = new SendableChooser<>();
 
+    AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(drivebase,
+                                                          // Applies deadbands and inverts controls because joysticks
+                                                          // are back-right positive while robot
+                                                          // controls are front-left positive
+                                                          () -> MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                       OperatorConstants.LEFT_Y_DEADBAND),
+                                                          () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                       OperatorConstants.LEFT_X_DEADBAND),
+                                                          () -> -driverXbox.getRightX(),
+                                                          () -> -driverXbox.getRightY());
 
+    AbsoluteFieldDrive closedFieldAbsoluteDrive = new AbsoluteFieldDrive(drivebase,
+                                                                         () ->
+                                                                             MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                                    OperatorConstants.LEFT_Y_DEADBAND),
+                                                                         () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                                      OperatorConstants.LEFT_X_DEADBAND),
+                                                                         () -> driverXbox.getRawAxis(2));
+
+    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
+                                                                      () -> MathUtil.applyDeadband(driverXbox.getLeftY() / -4.0,
+                                                                                                OperatorConstants.LEFT_Y_DEADBAND),
+                                                                      () -> MathUtil.applyDeadband(driverXbox.getLeftX() / -4.0,
+                                                                                                  OperatorConstants.LEFT_X_DEADBAND),
+                                                                      () -> MathUtil.applyDeadband(driverXbox.getRightX() / 4.0,
+                                                                                                  OperatorConstants.RIGHT_X_DEADBAND), 
+                                                                      driverXbox::getYButtonPressed, 
+                                                                      driverXbox::getAButtonPressed, 
+                                                                      driverXbox::getXButtonPressed, 
+                                                                      driverXbox::getBButtonPressed);
+
+    TeleopDrive simClosedFieldRel = new TeleopDrive(drivebase,
+                                                    () -> MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                 OperatorConstants.LEFT_Y_DEADBAND),
+                                                    () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                 OperatorConstants.LEFT_X_DEADBAND),
+                                                    () -> driverXbox.getRawAxis(2), () -> true);
+    TeleopDrive closedFieldRel = new TeleopDrive(
+        drivebase,
+        () -> MathUtil.applyDeadband(driverController.getRawAxis(1), OperatorConstants.LEFT_Y_DEADBAND),
+        () -> MathUtil.applyDeadband(driverController.getRawAxis(0), OperatorConstants.LEFT_X_DEADBAND),
+        () -> -driverController.getRawAxis(2), () -> true);
+
+    drivebase.setDefaultCommand(!RobotBase.isSimulation() ? closedAbsoluteDrive : closedFieldAbsoluteDrive);
   }
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
    */
-  private void configureBindings() {
-    new JoystickButton(driverJoystick, OIConstants.kDriverResetGyroButtonIdx).
-    onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading()));
+  private void configureBindings()
+  {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    new JoystickButton(driverXbox, 1).onTrue((new InstantCommand(drivebase::zeroGyro)));
+    new JoystickButton(driverXbox, 3).onTrue(new InstantCommand(drivebase::addFakeVisionReading));
+//    new JoystickButton(driverXbox, 3).whileTrue(new RepeatCommand(new InstantCommand(drivebase::lock, drivebase)));
   }
 
   /**
@@ -66,15 +115,19 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+  public Command getAutonomousCommand()
+  {
     // An example command will be run in autonomous
-    return m_chooser.getSelected();
+    return drivebase.getAutonomousCommand("New Path", true);
   }
 
-public AHRS getGyro() {
-    return gyro;
-}
-public SwerveSubsystem getSwerveSS() {
-  return swerveSubsystem;
+  public void setDriveMode()
+  {
+    //drivebase.setDefaultCommand();
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
   }
 }
