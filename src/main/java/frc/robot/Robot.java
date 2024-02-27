fdf8d9cb0e43b7bb62cf5b5fdf95b8193bc985a5
AbbeySieg <suvanzala365@gmail.com>
2024-02-26 18:16:10 -0600

    Added Distance to Apriltag reading

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -125,13 +125,15 @@ public class Robot extends TimedRobot
       default:
         // Put default auto code here
         break;
+    }
+    
          m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-  }
+  
     }
 
   /**
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -7,15 +7,13 @@ package frc.robot.subsystems;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-
 import edu.wpi.first.apriltag.AprilTagDetector;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -111,42 +109,29 @@ public PhotonTrackedTarget getBestTarget() {
         return hasTarget; // Returns whether or not a target was found
     }
 
-    public double getDistanceToTarget(PhotonTrackedTarget target) {
-        if (!hasTarget) {
-            return 0;
-        }
-        double april_tag_pitch = target.getPitch();
-        double april_tag_area = target.getArea();
-
-        double distance = april_tag_area;
-
-        // Print the area and pitch of the target
-        //System.out.println("Area: " + april_tag_height + "Pitch: " + april_tag_pitch);
-        SmartDashboard.putNumber("t_area", april_tag_area);
-        SmartDashboard.putNumber("t_pitch", april_tag_pitch);
-        return distance;
-    }
-
-    public boolean InRange(double distanceThreshold, double distanceThresholdRange,
+    public double distanceToTarget(double tagHeight, double cameraHeight, double cameraAngle, double distanceThreshold, double distanceThresholdRange,
     double angleThreshold, double angleThresholdRange) {
-        if (!hasTarget) {
-            return false;
-        }
-    
-        PhotonTrackedTarget bestTarget = getBestTarget();
-        double distanceToTarget  = getDistanceToTarget(bestTarget);
-        double angleToTarget = bestTarget.getYaw(); // Assuming yaw gives the angle
-        double skewTarget = bestTarget.getSkew();
+    // convert angle to radians
+    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(50);
+    final double TARGET_HEIGHT_METERS = Units.inchesToMeters(57.13);
+    // Angle between horizontal and the camera.
+    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
-        //boolean inRange = Math.abs(distanceToTarget) <= distanceThreshold && Math.abs(angleToTarget) <= angleThreshold;
-        boolean inRange = Math.abs(Math.abs(distanceToTarget) - distanceThreshold) >= distanceThresholdRange && Math.abs(Math.abs(angleToTarget) - angleThreshold) >= angleThresholdRange;
-        
-        SmartDashboard.putNumber("t_distance", distanceToTarget);
-        SmartDashboard.putNumber("t_angle", angleToTarget);
-        SmartDashboard.putNumber("t_skew", skewTarget);
-        SmartDashboard.putBoolean("InRange", inRange);
+    double angleRadians = Math.toRadians(cameraAngle);
+    double distance = (tagHeight - cameraHeight) / Math.tan(angleRadians);
+
+    if (result.hasTargets()) {
+                // First calculate range
+                double range =
+                        PhotonUtils.calculateDistanceToTargetMeters(
+                                CAMERA_HEIGHT_METERS,
+                                TARGET_HEIGHT_METERS,
+                                CAMERA_PITCH_RADIANS, 
+                                Units.degreesToRadians(result.getBestTarget().getPitch())); 
     
-        return inRange;
+    SmartDashboard.putNumber("range", range);
+    return distance;  }
+    return 0;
     }
 
 @Override
@@ -158,6 +143,5 @@ public PhotonTrackedTarget getBestTarget() {
         if (hasTarget) {
             this.result = result;
         }
-        InRange(0, 5, 0, 5); // Put to SmartDashboard whether or not the target is in range
-    }
+  }
 }
