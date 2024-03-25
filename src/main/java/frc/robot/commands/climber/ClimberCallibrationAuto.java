0973cbe2c0d8ebbc38ff4ec3aadacd72ee417c12
X-EX3-X <ouranous64@gmail.org>
2024-03-25 16:41:34 -0500

    Currently it looks like the ClimberAuto command isn't needed for the climber callibration auto, but I'm keeping it for now incase we need it in the future

--- src/main/java/frc/robot/commands/climber/ClimberCallibrationAuto.java
+++ src/main/java/frc/robot/commands/climber/ClimberCallibrationAuto.java
@@ -1,3 +1,6 @@
+//Currently it does not seem like this command is needed, but I'm not deleting it incase it has an actual use in the future
+
+
 package frc.robot.commands.climber;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 
@@ -23,16 +26,16 @@ public void initialize() {}
 
 @Override 
 public void execute() {
-    if (this.speed != 0 && !climber.leftHitLimitSwitch()) {
-        climber.leftClimbspeed(speed);
+    if (this.speed < 0 && climber.leftHitLimitSwitch()) { //This only works for if the climber is at its lowest, remember to add another condition for if it is at its highest
+        climber.leftClimbspeed(0);
     } else {
-    climber.leftClimbspeed(0);
+        climber.leftClimbspeed(speed);
     }
 
-    if (this.speed != 0 && !climber.rightHitLimitSwitch()) {
-        climber.rightClimbspeed(speed);
+    if (this.speed < 0 && climber.rightHitLimitSwitch()) {
+        climber.rightClimbspeed(0);
     } else {
-    climber.rightClimbspeed(0);
+        climber.rightClimbspeed(speed);
     }
   }
 
