ab0dec04bc05f616175314fbb99bdd400a06a8ee
X-EX3-X <ouranous64@gmail.org>
2024-03-30 10:23:29 -0500

    Shell of the code, doesn't work yet

--- src/main/java/frc/robot/commands/intake/SetIntakeCommand.java
+++ src/main/java/frc/robot/commands/intake/SetIntakeCommand.java
@@ -18,14 +18,25 @@ public class SetIntakeCommand extends Command {
 
     @Override
     public void execute() {
-        if (this.speed > 0 && !intake.hasNote()) { //if the intake does not have the note
-            intake.setSpeed(speed);     //then run the indexer and intake 
-            intake.indexerSpeed(speed);
 
-        } else {    //otherwise
+        if (this.speed > 0) {   //if you actually set a speed
+            
+            if (!intake.hasNote()) { //and the intake does not have the note
+                intake.setSpeed(speed);     //then run the indexer and intake 
+                intake.indexerSpeed(speed);
+            } else if (/* 2nd Switch Hit & Ange < 42*/) { //if the note is far up enough in the intake
+                intake.setSpeed(speed / 2);     //then slow down the speed
+                intake.indexerSpeed(speed / 2);
+            } else {    //if no switch is hit
+                intake.setSpeed(0);     //set the speed to 0
+                intake.indexerSpeed(0);
+            }
+
+        } else {    //if you didn't set any speed
             intake.setSpeed(0);       //the intake and indexer should be off
             intake.indexerSpeed(0);
         }
+
     }
 
 @Override
